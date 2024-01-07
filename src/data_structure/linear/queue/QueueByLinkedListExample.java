b434299689b70e16d5ae33ba19014aed678e454e
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-08 04:37:04 +0900

    [queue] 커스텀 용량 큐 테스트 케이스 추가

--- src/data_structure/linear/queue/QueueByLinkedListExample.java
+++ src/data_structure/linear/queue/QueueByLinkedListExample.java
@@ -6,6 +6,7 @@ public class QueueByLinkedListExample {
     public static void main(String[] args) {
         QueueByLinkedListCustom<Integer> queue = new QueueByLinkedListCustom<>();
         queue.getSize();
+//        queue.insert(0, 0);
 //        queue.poll();
 
         System.out.println();
@@ -54,5 +55,59 @@ public class QueueByLinkedListExample {
         System.out.println("The output Data : " + pollData);
         queue.getSize();
         queue.printAllNodes();
+
+        System.out.println();
+        System.out.println("----------------------");
+        System.out.println();
+
+        QueueByLinkedListCustom<String> anotherQueue =
+                new QueueByLinkedListCustom<>(15);
+
+        anotherQueue.getSize();
+//        anotherQueue.poll();
+
+        System.out.println();
+
+        anotherQueue.add("A");
+        anotherQueue.getSize();
+        anotherQueue.printAllNodes();
+
+        System.out.println();
+        anotherQueue.add("B");
+        anotherQueue.add("C");
+        anotherQueue.add("E");
+        anotherQueue.add("F");
+        anotherQueue.add("G");
+        anotherQueue.getSize();
+        anotherQueue.printAllNodes();
+
+        System.out.println();
+        anotherQueue.add("H");
+        anotherQueue.add("H");
+        anotherQueue.add("H");
+        anotherQueue.add("H");
+        anotherQueue.getSize();
+        anotherQueue.printAllNodes();
+
+        System.out.println();
+        anotherQueue.add("H");
+        anotherQueue.add("H");
+        anotherQueue.add("H");
+        anotherQueue.add("H");
+        anotherQueue.add("H");
+        anotherQueue.add("I");
+        anotherQueue.getSize();
+        anotherQueue.printAllNodes();
+
+        System.out.println();
+        String outputData = anotherQueue.poll();
+        System.out.println("The output Data : " + outputData);
+        anotherQueue.getSize();
+        anotherQueue.printAllNodes();
+
+        System.out.println();
+        anotherQueue.add("I");
+        anotherQueue.getSize();
+        anotherQueue.printAllNodes();
     }
 }
