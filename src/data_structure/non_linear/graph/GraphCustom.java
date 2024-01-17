4b858e714fe18845c8d7cd40974b403a45d09a06
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-17 13:15:22 +0900

    [graph] 자료별 알고리즘 클래스 접근을 위한 접근제한자 제약 완전 해제 - 주석 참조

--- src/data_structure/non_linear/graph/GraphCustom.java
+++ src/data_structure/non_linear/graph/GraphCustom.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 //TODO : 무방향 그래프
 public class GraphCustom<T> {
-    private List<Vertex<T>> vertices; // 정점들의 집합
+    public List<Vertex<T>> vertices; // 정점들의 집합
     // 모든 간선들의 집합은 단위 정점 클래스의 edges 필드를 getter로 가져와 출력?
     // 각 사이즈(정점 집합, 간선 집합)는 List의 메소드 활용으로 출력
 
@@ -136,16 +136,16 @@ public class GraphCustom<T> {
     }
 
     //TODO : 데이터 단위 정점을 중첩 클래스로 선언
-    private static class Vertex<T> {
-        private T data;
-        private List<Vertex<T>> edges; // 단위 정점에 연결된 간선들의 집합
+    public static class Vertex<T> {
+        public T data;
+        public List<Vertex<T>> edges; // 단위 정점에 연결된 간선들의 집합
 
-        private Vertex(T data) {
+        public Vertex(T data) {
             this.data = data;
             this.edges = new ArrayList<>();
         }
 
-        private void setData(T data) {
+        public void setData(T data) {
             this.data = data;
         }
     }
