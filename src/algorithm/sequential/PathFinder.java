18ccf156f90aedb872348a6b3679608cd97c48a2
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-17 11:49:34 +0900

    [PathFinder] 여러 그래프 인스턴스를 받기 위한 와일드카드 활용

--- src/algorithm/sequential/PathFinder.java
+++ src/algorithm/sequential/PathFinder.java
@@ -1,9 +1,16 @@
 package algorithm.sequential;
 
+import data_structure.non_linear.graph.GraphCustom;
+
 public class PathFinder {
     // TODO : 필드
+    private GraphCustom<?> graph;
 
     // TODO : 생성자
+
+    public PathFinder(GraphCustom<?> graph) {
+        this.graph = graph;
+    }
 
     // TODO : 메소드
 }
