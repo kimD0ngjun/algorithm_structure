a1d29454ade636b80764b718a1a37e91c179c896
kimD0ngjun <chickentasty0112@gmail.com>
2024-01-24 17:31:30 +0900

    [tree] 중첩 노드 클래스 작성

--- src/data_structure/non_linear/tree/TreeCustom.java
+++ src/data_structure/non_linear/tree/TreeCustom.java
@@ -3,4 +3,12 @@ package data_structure.non_linear.tree;
 import data_structure.non_linear.graph.GraphCustom;
 
 public class TreeCustom<T> extends GraphCustom<T> {
+    //TODO : 데이터 단위 정점을 중첩 클래스로 선언
+    // 그래프 클래스의 중첩 정점 클래스를 상속받아 변형
+    public static class Node<T> extends Vertex<T> {
+        // 간선은 자식 노드만 담도록 하기
+        public Node(T data) {
+            super(data);
+        }
+    }
 }
