a7f8f793aef6b6a9d08200d0eb3be3d310a5e6de
kimD0ngjun <chickentasty0112@gmail.com>
2024-02-07 16:13:35 +0900

    [LinkedList] 단방향 연결 리스트 취지에 맞춰서 리팩토링

--- src/data_structure/linear/linkedList/linked_list.py
+++ src/data_structure/linear/linkedList/linked_list.py
@@ -8,3 +8,25 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+# 단방향 연결 리스트
+class LinkedList:
+    def __init__(self, head=None, tail=None):
+        self.head = head
+        self.tail = tail # 연결 리스트의 취지에 맞게
+
+    def append(self, val):
+        new_node = ListNode(val)
+
+        # 처음 노드를 추가할 경우
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+            return
+
+        # 기존의 꼬리에 덧붙이는 경우
+
+        # 기존 꼬리 노드의 None이었던 next를 정해줘야 함
+        self.tail.next = new_node
+        # 새로운 꼬리 노드를 지정함
+        self.tail = new_node
