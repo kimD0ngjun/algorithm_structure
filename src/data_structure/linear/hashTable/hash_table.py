102d5453d2b6c5e4f05bfa887ce2ab7371f1898f
kimD0ngjun <chickentasty0112@gmail.com>
2024-02-10 19:49:36 +0900

    [Hash] 해시 노드 생성

--- src/data_structure/linear/hashTable/hash_table.py
+++ src/data_structure/linear/hashTable/hash_table.py
@@ -1,7 +1,9 @@
 # 해시에 저장될 단위 노드
 class HashNode:
-    def __init__(self):
-        pass
+    def __init__(self, idx: int, value: str, next_node: 'HashNode' = None):
+        self.idx = idx
+        self.value = value
+        self.next_node = next_node
 
 # 커스텀 해시
 class HashCustom:
