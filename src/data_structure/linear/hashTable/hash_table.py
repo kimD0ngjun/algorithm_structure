60a50172508d8af3b62a3c6cc0d3624158e9594e
kimD0ngjun <chickentasty0112@gmail.com>
2024-02-10 20:14:16 +0900

    [Hash] 해시 클래스 작성 메소드 정의

--- src/data_structure/linear/hashTable/hash_table.py
+++ src/data_structure/linear/hashTable/hash_table.py
@@ -1,11 +1,22 @@
+from typing import Optional, List
+
 # 해시에 저장될 단위 노드
 class HashNode:
-    def __init__(self, idx: int, value: str, next_node: 'HashNode' = None):
+    def __init__(self, idx: int, value: str, next_node=None):
         self.idx = idx
         self.value = value
         self.next_node = next_node
 
 # 커스텀 해시
 class HashCustom:
-    def __init__(self):
-        pass
+    BUCKET_SIZE = 97
+
+    # 생성자
+    def __init__(self, bucket):
+        self.bucket = [None] * HashCustom.BUCKET_SIZE
+
+    # 삽입 및 업데이트
+
+    # 조회
+
+    # 삭제
