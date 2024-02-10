8a00e96b301f085c429410bcdea59c4f8c37da7d
kimD0ngjun <chickentasty0112@gmail.com>
2024-02-10 20:54:12 +0900

    [Hash] 삽입 메소드 교정

--- src/data_structure/linear/hashTable/hash_table.py
+++ src/data_structure/linear/hashTable/hash_table.py
@@ -18,29 +18,35 @@ class HashCustom:
 
     # 삽입 및 업데이트
     def custom_set(self, key: str, value: str) -> None:
-    # 각 문자열의 문자들의 아스키코드 값을 더한 값을 해시 함수에 인자로 넣는다.
+        # 각 문자열의 문자들의 아스키코드 값을 더한 값을 해시 함수에 인자로 넣는다.
         ascii_sum = sum(ord(char) for char in key)
         idx = ascii_sum % HashCustom.BUCKET_SIZE
 
         # 인덱스에 저장이 안 됐을 경우
         if self.bucket[idx: int] is None:
-            self.bucket[idx: int] = value
+            self.bucket[idx: int] = HashNode(key, idx, value)
             return
+        # 인덱스에 이미 저장이 되어있는 경우
         else:
-            if self.bucket[idx].key == key:
-                self.bucket[idx].value = value
-                return
-            else:
+            # 개별 체이닝 탐색 시작
+            while node is not None:
                 node = self.bucket[idx]
-                while node is not None:
-                    if node.key == key:
-                        node.value = value
-                        return
-                    else:
-                        node.next_node = HashNode(key, idx, value)
 
-                    node = node.next_node
+                # 만약 리스트의 노드 중 같은 key를 찾으면 업데이트
+                if node.key == key:
+                    node.value = value
+                    return
+
+                # 아니라면 가장 마지막 node에 연결 리스트 삽입
+                if node.next_node is None:
+                    node.next_node = HashNode(key, idx, value)
+                    return
+
+                node = node.next_node
 
     # 조회
+    def get_custom(self, key: str) -> int:
+        ascii_sum = sum(ord(char) for char in key)
+        idx = ascii_sum % HashCustom.BUCKET_SIZE
 
     # 삭제
