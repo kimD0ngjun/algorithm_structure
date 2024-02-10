56fec64f52feb783979bc15d565263350b391e8c
kimD0ngjun <chickentasty0112@gmail.com>
2024-02-10 20:38:40 +0900

    [Hash] 삽입 및 업데이트 로직 완료

--- src/data_structure/linear/hashTable/hash_table.py
+++ src/data_structure/linear/hashTable/hash_table.py
@@ -2,7 +2,8 @@ from typing import Optional, List
 
 # 해시에 저장될 단위 노드
 class HashNode:
-    def __init__(self, idx: int, value: str, next_node=None):
+    def __init__(self, key: str, idx: int, value: str, next_node=None):
+        self.key = key
         self.idx = idx
         self.value = value
         self.next_node = next_node
@@ -20,7 +21,25 @@ class HashCustom:
     # 각 문자열의 문자들의 아스키코드 값을 더한 값을 해시 함수에 인자로 넣는다.
         ascii_sum = sum(ord(char) for char in key)
         index = ascii_sum % HashCustom.BUCKET_SIZE
-        self.bucket[index: int] = value
+
+        # 인덱스에 저장이 안 됐을 경우
+        if self.bucket[index: int] is None:
+            self.bucket[index: int] = value
+            return
+        else:
+            if self.bucket[index].key == key:
+                self.bucket[index].value = value
+                return
+            else:
+                node = self.bucket[index]
+                while node is not None:
+                    if node.key == key:
+                        node.value = value
+                        return
+                    else:
+                        node.next_node = HashNode(key, index, value)
+
+                    node = node.next_node
 
     # 조회
 
