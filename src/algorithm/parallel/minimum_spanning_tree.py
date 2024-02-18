cdf4550dd756ed0238a09bac199152b890f1ac16
kimD0ngjun <chickentasty0112@gmail.com>
2024-02-18 11:26:54 +0900

    [MST] 프림 알고리즘 리팩토링

--- src/algorithm/parallel/minimum_spanning_tree.py
+++ src/algorithm/parallel/minimum_spanning_tree.py
@@ -14,52 +14,20 @@ def prim(graph, start_vertex):
     # 그래프 세팅
     # 이중 리스트로 내부 리스트에 (인접 정점, 가중치) 튜플로 간선 정보 표기
     # 그 중, 최소 신장 트리만을 추출하기
-    mst_graph: Dict[int, List[Tuple[int, int]]] = {i: [] for i in range(len(graph))}
+    mst_graph = {i: [] for i in range(len(graph))}
 
-    visited = set() # 방문 정보
+    visited = set() # 간선 방문 정보
     heap = [] # 최소 힙
 
-    # (가중치, 정점) 튜플 식으로 할당
+    # (정점, 가중치) 튜플 식으로 할당
     # 튜플의 첫 번째 요소(여기서는 가중치)를 기준으로 최소 힙이 구성
     heapq.heappush(heap, (0, start_vertex))
 
     while heap:
-        # 가중치를 기준으로 한 최소 튜플(heapq.heappop(heap))의 정점([1]) 추출
-        cur_min_vertex = heapq.heappop(heap)[1]
+        # 가중치를 기준으로 한 최소 튜플(heapq.heappop(heap))의 정점([0])과 가중치([1]) 추출
+        cur_weight, cur_vertex = heapq.heappop(heap)
 
-        # 백트랙킹
-        if cur_min_vertex in visited:
-            continue
 
-        visited.add(cur_min_vertex) # 방문 기록
-
-        # # 최소 가중치 및 해당 최소 정점(즉 간선을 구성하는) 초기화
-        min_weight = sys.maxsize
-        min_vertex = None
-
-        # 인접 간선 정보들 순회
-        for adj_vertex, cost in graph[cur_min_vertex]:
-            # 이미 탑색이 끝난 정점은 백트랙킹
-            # 순환형 재탐색 방지
-            if adj_vertex in visited:
-                continue
-
-            # 현재까지의 최소 가중치와 그 가중치를 가진 간선 갱신
-            if cost < min_weight:
-                min_weight = cost
-                min_vertex = adj_vertex
-
-                # 최소 가중치 간선 방문 기록 추가
-                visited.add(min_vertex)
-
-                # 최소 신장 트리에 업뎃
-                mst_graph[cur_min_vertex].append((min_vertex, min_weight))
-                mst_graph[min_vertex].append((cur_min_vertex, min_weight))
-
-                 # 해당 인접 정점과 연결된 간선 정보를 힙에 추가
-                heapq.heappush(heap, (min_weight, min_vertex))
-
-        return mst_graph
 
 graph = {
     0: [(1, 8), (5, 15)],  # 정점 0에 대한 간선 정보
