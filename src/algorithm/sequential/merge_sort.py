eb8bcbba46c2fbc3a5cfcca252c6b4247ff89833
kimD0ngjun <chickentasty0112@gmail.com>
2024-02-22 00:16:18 +0900

    [2.22] 병합 정렬 임시 배열 클래스 인스턴스화

--- src/algorithm/sequential/merge_sort.py
+++ src/algorithm/sequential/merge_sort.py
@@ -1,6 +1,7 @@
 class MergeSort:
     def __init__(self, array):
         self.array = array
+        self.temp = [0] * len(array)  # 임시 배열을 인스턴스 변수로 초기화
 
     def merge_sort(self):
         self.sort(0, len(self.array) - 1)
@@ -23,7 +24,6 @@ class MergeSort:
 
     # 별도의 배열화
     def merge(self, low_index, middle_index, high_index):
-        temp = [0] * (high_index - low_index + 1)  # 임시로 담을 배열
         temp_index = 0  # 임시 배열 인덱스
         left_pointer = low_index  # 쪼갠 배열 중 한쪽 포인터
         right_pointer = middle_index + 1  # 쪼갠 배열 중 다른쪽 포인터
@@ -32,30 +32,30 @@ class MergeSort:
         while left_pointer <= middle_index and right_pointer <= high_index:
             if self.array[left_pointer] <= self.array[right_pointer]:
                 # 배열에 할당시킴과 동시에 변수 증가 업데이트(외워두기)
-                temp[temp_index] = self.array[left_pointer]
+                self.temp[temp_index] = self.array[left_pointer]
                 temp_index += 1
                 left_pointer += 1
             else:
-                temp[temp_index] = self.array[right_pointer]
+                self.temp[temp_index] = self.array[right_pointer]
                 temp_index += 1
                 right_pointer += 1
 
         # 한쪽 포인터가 먼저 다다랐을 때 수행
         while left_pointer <= middle_index:
             # 잔여 배열들 전부 붙여넣기
-            temp[temp_index] = self.array[left_pointer]
+            self.temp[temp_index] = self.array[left_pointer]
             temp_index += 1
             left_pointer += 1
 
         # 얘도 마찬가지
         while right_pointer <= high_index:
-            temp[temp_index] = self.array[right_pointer]
+            self.temp[temp_index] = self.array[right_pointer]
             temp_index += 1
             right_pointer += 1
 
         # 임시 배열의 내용을 원래 배열에 붙여넣기
-        for i in range(len(temp)):
-            self.array[low_index + i] = temp[i]
+        for i in range(temp_index):
+            self.array[low_index + i] = self.temp[i]
 
 # 테스트
 if __name__ == "__main__":
